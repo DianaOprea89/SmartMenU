cdf59a153be24df8de3d176295211e6d4bcc8c5d
DianaO <106057837+DianaOprea89@users.noreply.github.com>
2023-12-13 16:28:30 +0200

    -worked on displaying the mealOption

--- front-end/src/components/SubMenuOption.vue
+++ front-end/src/components/SubMenuOption.vue
@@ -14,31 +14,31 @@
       </meal-option>
 
     </div>
-    <div v-if="restaurantData && restaurantData.subMenuOptions">
-      <div v-for="(subMenuOption, index) in restaurantData.subMenuOptions" :key="index" class="menu-option">
+    <div class="menu-container" v-if="restaurantData && restaurantData.subMenuOptions">
+      <div v-for="(subMenuOption, index) in restaurantData.subMenuOptions" :key="index">
         <div class="menu-option-content m-5" v-if="subMenuOption">
           <div class="menu-option-row">
             <img :src="subMenuOption.photoLink" alt="Menu item" class="menu-option-image">
             <p class="menu-option-name">{{ subMenuOption.subMenuOptionName }}</p>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil edit-icon"
                  viewBox="0 0 16 16" @click="editOption(subMenuOption)">
-                  <path
-                      d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+              <path
+                  d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
             </svg>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16" @click="removeSubMenuItem(subMenuOption._id)">
               <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
             </svg>
-            <ul>
-              <li>
-                <div v-for="mealOption in subMenu.mealOptions" :key="mealOption._id">
-                <h3>{{ mealOption.name }} ({{ mealOption.quantity }})</h3>
-                <p>{{ mealOption.ingredients }}</p>
-                <p>{{ mealOption.description }}</p>
-                <span>{{ mealOption.price }}</span>
-              </div>
-              </li>
-            </ul>
-
+          </div>
+        </div>
+        <div class="meal-options menu-option-row">
+          <div v-for="mealOption in subMenuOption.mealOptions" :key="mealOption._id" class="meal-option-card">
+           <img :src="mealOption.photoLink" alt="Menu item" class="menu-option-image">
+            <h3>{{ mealOption.optionName }} ({{ mealOption.quantity }} {{ mealOption.unit }})</h3>
+            <div class="meal-details">
+              <p><strong>Ingredients:</strong> {{ mealOption.ingredients }}</p>
+              <p><strong>Description:</strong> {{ mealOption.description }}</p>
+              <span class="meal-price">{{ mealOption.price }} RON</span>
+            </div>
           </div>
         </div>
       </div>
@@ -259,6 +259,37 @@ export default {
 
 
 <style scoped>
+.menu-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.meal-options {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  gap: 20px;
+}
+
+.meal-option-card {
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 15px;
+  background-color: #f9f9f9;
+}
+
+.meal-option-card h3 {
+  margin-top: 0;
+  color: #333;
+}
+
+.meal-details {
+  margin-bottom: 10px;
+}
+
+.meal-price {
+  font-weight: bold;
+  color: #d9534f;
+}
 .custom-dialog {
   position: fixed;
   top: 50%;
@@ -270,9 +301,13 @@ export default {
   z-index: 1000;
   padding: 20px;
   border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 }
-
+.custom-dialog-content input[type="text"] {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+}
 .menu-option-row {
   display: flex;
   align-items: center;
@@ -290,20 +325,6 @@ export default {
   font-size: 1em;
   font-weight: bold;
   margin: 0;
-}
-
-.edit-icon {
-  cursor: pointer;
-}
-
-.remove-button {
-  padding: 5px 10px;
-}
-
-.custom-dialog-content input[type="text"] {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
 }
 </style>
 
