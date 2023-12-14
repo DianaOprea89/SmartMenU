79082fd6a342ba20eea1e0a0c73a18b71ad444e0
DianaO <106057837+DianaOprea89@users.noreply.github.com>
2023-12-14 12:35:22 +0200

    -working on correcting the way the mealOption are being displayed (css and html classes)

--- front-end/src/components/SubMenuOption.vue
+++ front-end/src/components/SubMenuOption.vue
@@ -12,36 +12,43 @@
           :sub-menu-options="restaurantData.subMenuOptions"
           @meal-option-added="handleMealOptionAdded">
       </meal-option>
-
     </div>
-    <div class="menu-container" v-if="restaurantData && restaurantData.subMenuOptions">
-      <div v-for="(subMenuOption, index) in restaurantData.subMenuOptions" :key="index">
-        <div class="menu-option-content m-5" v-if="subMenuOption">
-          <div class="menu-option-row">
+    <div class="menu-layout" v-if="restaurantData && restaurantData.subMenuOptions">
+      <aside class="menu-sidebar">
+        <ul class="submenu-list">
+          <li v-for="(subMenuOption, index) in restaurantData.subMenuOptions" :key="index">
             <img :src="subMenuOption.photoLink" alt="Menu item" class="menu-option-image">
-            <p class="menu-option-name">{{ subMenuOption.subMenuOptionName }}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil edit-icon"
+            <p class="sub-menu-title">{{ subMenuOption.subMenuOptionName }}</p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil edit-icon m-4"
                  viewBox="0 0 16 16" @click="editOption(subMenuOption)">
               <path
                   d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
             </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16" @click="removeSubMenuItem(subMenuOption._id)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3 m-4" viewBox="0 0 16 16" @click="removeSubMenuItem(subMenuOption._id)">
               <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
             </svg>
-          </div>
+          </li>
+        </ul>
+
+      </aside>
+      <main class="menu-main-content">
+        <div v-for="(subMenuOption, index) in restaurantData.subMenuOptions" :key="index">
+          <ul class="meal-list">
+            <li v-for="mealOption in subMenuOption.mealOptions" :key="mealOption._id" class="meal-item">
+              <img :src="mealOption.photoLink" alt="Meal image" class="meal-image">
+              <div class="meal-content">
+                <h3>{{ mealOption.optionName }}</h3>
+                <p class="meal-description">{{ mealOption.description }}</p>
+                <p class="meal-ingredients"><strong>Ingredients:</strong> {{ mealOption.ingredients }}</p>
+                <div class="meal-footer">
+                  <span class="meal-quantity">{{ mealOption.quantity }} {{ mealOption.unit }}</span>
+                  <span class="meal-price">{{ mealOption.price }} RON</span>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
-        <div class="meal-options menu-option-row">
-          <div v-for="mealOption in subMenuOption.mealOptions" :key="mealOption._id" class="meal-option-card">
-           <img :src="mealOption.photoLink" alt="Menu item" class="menu-option-image">
-            <h3>{{ mealOption.optionName }} ({{ mealOption.quantity }} {{ mealOption.unit }})</h3>
-            <div class="meal-details">
-              <p><strong>Ingredients:</strong> {{ mealOption.ingredients }}</p>
-              <p><strong>Description:</strong> {{ mealOption.description }}</p>
-              <span class="meal-price">{{ mealOption.price }} RON</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      </main>
     </div>
 <!--    Creating a custom dialog to add  a new SubMenuOption-->
     <div class="custom-dialog" v-if="showDialog">
@@ -106,6 +113,8 @@ export default {
       menuOptionId: '', // Initialize menuOptionId
       showDialog: false,
       showDialogOption: false,
+      activeSubMenu: null,
+      activeMealOptions: [],
       newShowDialog:false,
       newOptionSubMenu: { optionName: '', photoLink: '' }, // Initialize with default values
       editingSubMenuOption: {
@@ -134,7 +143,21 @@ export default {
       this.editingSubMenuOption._id = subMenuOption._id;
       this.showDialogOption = true;
     },
-
+    setActiveSubMenu(subMenuId) {
+      console.log('setActiveSubMenu called with id:', subMenuId);
+      this.activeSubMenu = subMenuId;
+      const subMenu = this.restaurantData.subMenuOptions.find(
+          (subMenuOption) => subMenuOption._id.$oid === subMenuId
+      );
+      if(subMenu) {
+        console.log('Active subMenu:', subMenu);
+        this.activeMealOptions = subMenu.mealOptions || [];
+        console.log('Active meal options:', this.activeMealOptions);
+      } else {
+        console.log('No subMenu found for id:', subMenuId);
+        this.activeMealOptions = [];
+      }
+    },
     addSubMenuItem() {
       const newSubMenuItem = {
         photoLink: this.subMenu.photoLink,
@@ -259,72 +282,116 @@ export default {
 
 
 <style scoped>
-.menu-container {
+.container {
   display: flex;
   flex-direction: column;
+  max-width: 1200px;
+  margin: auto;
 }
 
-.meal-options {
+.menu-layout {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
-  gap: 20px;
+  grid-template-columns: 1fr 3fr;
+  gap: 2rem;
+}
+.active {
+  background-color: #ddd; /* or any color you want for the active state */
+}
+.menu-sidebar {
+  background: #f9f9f9;
+  padding: 1rem;
 }
 
-.meal-option-card {
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 15px;
-  background-color: #f9f9f9;
+.submenu-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
 
-.meal-option-card h3 {
-  margin-top: 0;
-  color: #333;
+.submenu-list li {
+  padding: 0.5rem 0;
 }
 
-.meal-details {
-  margin-bottom: 10px;
+.menu-option-image {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 10px;
+}
+
+.sub-menu-title {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin: 0;
+}
+
+.menu-main-content {
+  background: #fff;
+  padding: 1rem;
+}
+
+.meal-list {
+  list-style: none;
+  padding: 0;
+}
+
+.meal-item {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+}
+
+.meal-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 5px;
+  object-fit: cover;
+  margin-right: 1rem;
+}
+
+.meal-content {
+  flex: 1;
+}
+
+.meal-description {
+  font-style: italic;
+  margin: 0.5rem 0;
+}
+
+.meal-ingredients {
+  margin: 0;
+}
+
+.meal-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1rem;
+}
+
+.meal-quantity {
+  font-size: 0.9rem;
 }
 
 .meal-price {
   font-weight: bold;
-  color: #d9534f;
+  color: #333;
 }
-.custom-dialog {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 60%;
-  max-width: 400px;
-  background-color: rgba(255, 255, 255, 0.9);
-  z-index: 1000;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-}
-.custom-dialog-content input[type="text"] {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-}
-.menu-option-row {
-  display: flex;
-  align-items: center;
-  gap: 10px;
+.submenu-list li {
+  padding: 0.5rem 0;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
 }
 
-.menu-option-image {
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-  border-radius: 5px;
+.active {
+  background-color: #ddd; /* or any other style you want */
+  /* other styles to make it look "active" */
+  cursor: pointer;
+  color: #000;
 }
 
-.menu-option-name {
-  font-size: 1em;
-  font-weight: bold;
-  margin: 0;
-}
 </style>
 
