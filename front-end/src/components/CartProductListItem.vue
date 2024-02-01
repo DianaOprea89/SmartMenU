10e243798e0c0718a18582fa6e708abc4879ad5a
DianaO <106057837+DianaOprea89@users.noreply.github.com>
2024-02-01 14:47:29 +0200

    -edited and fixed the component CarProductListItem

--- front-end/src/components/CartProductListItem.vue
+++ front-end/src/components/CartProductListItem.vue
@@ -1,26 +1,4 @@
-<template>
-  <div class="product-container">
-    <img class="product-image" :src="getImageUrl(product.product.imageUrl)">
-    <div class="details-wrap">
-      <h3>{{ product.product.name }}</h3>
-      <p>$ {{ product.product.price }}</p>
-    </div>
-    <div class="actions-wrap">
-      <span>Qty</span>
-      <p class="counting">{{ product.count }}</p>
 
-      <p >
-          <span>
-               <i class="arrow up" @click="handleAddtoCart(product.product.id)"></i>
-               <i class="arrow down"  @click="handleRemoveFromCart(product.product.id)"></i>
-         </span>
-      </p>
-      <button class="remove-button remove-text" @click="handleRemoveFromCart(product.product.id)">
-        Remove item from cart
-      </button>
-    </div>
-  </div>
-</template>
 
 
 <script>
